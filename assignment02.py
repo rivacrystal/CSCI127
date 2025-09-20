9c9e7ceacc6a8367f6eaa210d478207b82425681
Riva A. Crystal <92542234+rivacrystal@users.noreply.github.com>
2025-09-19 21:08:09 -0400

    Create assignment02.py

---
+++ assignment02.py
@@ -0,0 +1,15 @@
+#Name: Riva Crystal
+#Email: riva.crystal@gmail.com
+#Date: September 9, 2021
+#This program uses a turtle
+
+import turtle
+
+wn = turtle.Screen()
+thomasH = turtle.Turtle()
+
+for i in range(9):
+    thomasH.forward(100)
+    thomasH.left(40)
+
+wn.exitonclick()
